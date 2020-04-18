3d95558acf4ee9bde5df378cbdee475a0685a6bb
DmLvkvch <dima2231298@gmail.com>
2020-04-18 19:44:11 +0300

    pr6_3

---
+++ 7383_Levkovich_6_3/pr.py
@@ -0,0 +1,59 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+from var3 import gen_data
+import random
+
+samples, size = 500, 50
+data, labels = gen_data(samples, size)
+rand = list(range(len(data)))
+random.shuffle(rand)
+data = data[rand]
+labels = labels[rand]
+encoder = LabelEncoder()
+encoder.fit(labels)
+labels = encoder.transform(labels)
+labels = to_categorical(labels)
+
+data = data.reshape(data.shape[0], size, size, 1)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(size, size, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+H = model.fit(data, labels, batch_size=20, epochs=12, validation_split=0.2)
+
+loss = H.history['loss']
+val_loss = H.history['val_loss']
+acc = H.history['acc']
+val_acc = H.history['val_acc']
+epochs = range(1, len(loss) + 1)
+
+
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+plt.clf()
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
