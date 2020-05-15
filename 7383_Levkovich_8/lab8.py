b81f0eab445ab9a101cbaf52071e66760a5749f6
DmLvkvch <31544742+DmLvkvch@users.noreply.github.com>
2020-05-15 21:47:10 +0300

    Add files via upload

--- 7383_Levkovich_8/lab8.py
+++ 7383_Levkovich_8/lab8.py
@@ -80,7 +80,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint, CustomCB([1, 2, 5, 7, 11, 15, 19])]
+callbacks_list = [checkpoint, CustomCB([1, 2,3])]
 
 model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
 generate(model)
